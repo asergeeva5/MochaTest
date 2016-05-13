c0cdfd2023fabfab99150b3aa9f395d255a59007
Sasha <asergeeva5@gmail.com>
2016-05-12 20:43:25 -0600

    Initial files

---
+++ test/getPalette.test.js
@@ -0,0 +1,43 @@
+var getPalette = require("../lib/getPalette"); 
+var assert = require("assert");
+var fs  = require("fs");
+
+var configFile = process.cwd() + "/config.json";
+
+function writeConfig(config, callback) {
+  fs.writeFile(configFile, JSON.stringify(config), callback);
+}
+
+describe("getPalette", function(){
+  
+  var config = {};
+  
+  before(function(done) {
+  	fs.readFile(configFile, function(err, contents) {
+      if (err) {
+        console.error(err);
+      } else {
+        config = JSON.parse(contents.toString());
+      }
+      done();
+  	});
+  });
+
+   afterEach(function(done){
+   	 writeConfig(config, done);
+   });
+
+   it("should throw an error if the result is not an array", function(done){
+     //fs.writeFile(process.cwd() + "/config.json", {palette: "string"},function(err){
+     writeConfig({palette: "string"}, function(err){
+        assert.throws(getPalette, /is not an array/);
+        done();
+     });
+    });
+
+   it("should return an array with 3 items by default", function(){
+      var palette = getPalette();
+      assert(Array.isArray(palette, "did not return an array"));
+      assert.equal(palette.length, 3, "did not return 3 items");
+   });
+});
